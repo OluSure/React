4fa4347e76b35746945d5a5c272393f03bbe3ff3
OluSure <tobisco29@gmail.com>
2025-08-25 14:52:39 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,49 @@
 import './index.css';
 import Employee from './components/Employee';
 import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 
 function App() {
   const [role, setRole] = useState('dev');
+  const [employees, setEmployees] = useState(
+[
+  {
+    name: 'TObi',
+    role: 'Full Stack Developer',
+    img: 'https://images.pexels.com/photos/31585377/pexels-photo-31585377.jpeg?cs=srgb',
+  },
+    {
+    name: 'BObi',
+    role: 'Business Developer',
+    img: 'https://images.pexels.com/photos/31585377/pexels-photo-31585377.jpeg?cs=srgb',
+  },
+    {
+    name: 'CObi',
+    role: 'JavaScript Developer',
+    img: 'https://images.pexels.com/photos/31585377/pexels-photo-31585377.jpeg?cs=srgb',
+  },
+  {
+    name: 'DObi',
+    role: 'Product Developer',
+    img: 'https://images.pexels.com/photos/31585377/pexels-photo-31585377.jpeg?cs=srgb',
+  },
+  {
+    name: 'FObi',
+    role: 'Community Developer',
+    img: 'https://images.pexels.com/photos/31585377/pexels-photo-31585377.jpeg?cs=srgb',
+  },
+  {
+    name: 'GObi',
+    role: 'Web Developer',
+    img: 'https://images.pexels.com/photos/31585377/pexels-photo-31585377.jpeg?cs=srgb',
+  },
+  {
+    name: 'HObi',
+    role: 'Building Developer',
+    img: 'https://images.pexels.com/photos/31585377/pexels-photo-31585377.jpeg?cs=srgb',
+  },
+]);
 const showEmployees = true;
   return (
     <div className="App">
@@ -15,15 +54,18 @@ const showEmployees = true;
     setRole = (e.target.value);
   }}
   />
-  <div className="bg-red-300 flex flex-wrap">
-<Employee name="Tobfi" role="Intern" />
-<Employee name="jobi" role={role} />
-<Employee name="dobi" />
-<Employee name="Todi" role="Intern" />
-<Employee name="kobi" role={role} />
-<Employee name="pobi" />
-<Employee name="fobfi" role="Intern" />
-
+  <div className="bg-red-300 flex flex-wrap justify-center">
+{employees.map((employee) => {
+  console.log(uuidv4());
+   return ( 
+   <Employee
+   key={uuidv4()}
+   name={employee.name}
+   role={employee.role}
+   img={employee.img}
+   />
+);
+})}
 </div>
 </>
 ) : (
--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,15 +1,15 @@
+import EditEmployee from "./EditEmployee";
+
 function Employee(props) {
     return (
-<div class="m-2 p-8 sm:flex-row sm:items-center sm:gap-6 sm:py-4 ...">
-  <img class="mx-auto block h-24 rounded-full sm:mx-0 sm:shrink-0" src="/img/erin-lindford.jpg" alt="Image" />
-  <div class="space-y-2 text-center sm:text-left">
-    <div class="space-y-0.5">
-      <p class="text-lg font-semibold text-black">{props.name}</p>
-      <p class="font-medium text-gray-500">{props.role}</p>
+<div className="m-2 p-8 max-w-sm bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex">
+  <img className="oject-cover rounded-full h-[100px] w-[100px] block mx-auto block h-24 rounded-full sm:mx-0 sm:shrink-0" src={props.img} alt="Image" />
+  <div className="space-y-2 text-center sm:text-left">
+    <div className="space-y-0.5">
+      <p className="text-lg font-semibold text-black">{props.name}</p>
+      <p className="font-medium text-gray-500">{props.role}</p>
     </div>
-    <button class="border-purple-200 text-purple-600 hover:border-transparent hover:bg-purple-600 hover:text-white active:bg-purple-700 ...">
-      Message
-    </button>
+    <EditEmployee />
   </div>
 </div>
 
